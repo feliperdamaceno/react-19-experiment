1ebd7865ca82ab1ca56f0eb1fd04e114a4908038
feliperdamaceno <feliperdamaceno@sent.com>
2024-05-26 01:29:29 +0100

    :zap: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { RegisterForm } from './components'
+
+export default function App() {
+  return (
+    <main className="container flex-1 w-full mx-auto place-content-center">
+      <RegisterForm />
+    </main>
+  )
+}
---
+++ src/components/RegisterForm.tsx
@@ -0,0 +1,143 @@
+import z from 'zod'
+
+import { useActionState } from 'react'
+
+const schema = z.object({
+  firstName: z.string().max(100),
+  lastName: z.string().max(100),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: 'Password is too short' })
+    .max(20, { message: 'Password is too long' })
+})
+
+type Fields = z.infer<typeof schema>
+
+interface FormState {
+  success: boolean
+  errors: Partial<Record<keyof Fields, string>> | undefined
+  fields: Fields
+}
+
+async function registerFormAction(
+  state: FormState,
+  formData: FormData
+): Promise<FormState> {
+  const data = Object.fromEntries(formData)
+  const validation = schema.safeParse(data)
+
+  if (!validation.success) {
+    const errorMap = validation.error.flatten().fieldErrors
+    return {
+      ...state,
+      success: validation.success,
+      errors: {
+        firstName: errorMap.firstName?.[0],
+        lastName: errorMap.lastName?.[0],
+        email: errorMap.email?.[0],
+        password: errorMap.password?.[0]
+      }
+    }
+  }
+
+  return {
+    ...state,
+    success: validation.success,
+    errors: undefined,
+    fields: validation.data
+  }
+}
+
+const initialState: FormState = {
+  success: false,
+  errors: undefined,
+  fields: {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  }
+}
+
+export default function RegisterForm() {
+  const [state, action, isPending] = useActionState(
+    registerFormAction,
+    initialState
+  )
+
+  return (
+    <form
+      action={action}
+      className="flex flex-col w-full max-w-lg gap-8 p-8 mx-auto border rounded-sm shadow-sm"
+    >
+      <h2 className="text-2xl font-semibold">Create an account</h2>
+
+      <div className="space-y-4">
+        <label className="flex flex-col gap-2">
+          <span className="font-medium">First Name</span>
+          <input
+            className="px-2 py-1 border rounded-sm"
+            type="text"
+            name="firstName"
+            defaultValue={state.fields.firstName}
+            required
+          />
+          {state.errors?.firstName ? (
+            <span className="text-red-500">{state.errors.firstName}</span>
+          ) : null}
+        </label>
+
+        <label className="flex flex-col gap-2">
+          <span className="font-medium">Last Name</span>
+          <input
+            className="px-2 py-1 border rounded-sm"
+            type="text"
+            name="lastName"
+            defaultValue={state.fields.lastName}
+            required
+          />
+          {state.errors?.lastName ? (
+            <span className="text-red-500">{state.errors.lastName}</span>
+          ) : null}
+        </label>
+
+        <label className="flex flex-col gap-2">
+          <span className="font-medium">Email</span>
+          <input
+            className="px-2 py-1 border rounded-sm"
+            type="email"
+            name="email"
+            defaultValue={state.fields.email}
+            required
+          />
+          {state.errors?.email ? (
+            <span className="text-red-500">{state.errors.email}</span>
+          ) : null}
+        </label>
+
+        <label className="flex flex-col gap-2">
+          <span className="font-medium">Password</span>
+          <input
+            className="px-2 py-1 border rounded-sm"
+            type="password"
+            name="password"
+            defaultValue={state.fields.password}
+            required
+          />
+          {state.errors?.password ? (
+            <span className="text-red-500">{state.errors.password}</span>
+          ) : null}
+        </label>
+      </div>
+
+      <button
+        className="px-3 py-1.5 rounded-sm hover:scale-[102%] active:scale[98%] transition-transform bg-zinc-800 text-white w-fit"
+        disabled={isPending}
+        type="submit"
+      >
+        Register
+      </button>
+    </form>
+  )
+}
